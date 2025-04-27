5a77f0fc5026fd8d39fb010aa696eb647261d7c4
sneha280904 <sneha280904@gmail.com>
2025-04-28 00:02:08 +0530

    initial commit

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 import streamlit as st
+from dotenv import load_dotenv
 import os
 from util import *
 
+# Load environment variables from the .env file
+load_dotenv()
 
 def main():
+    response = ""
+
     st.set_page_config(page_title="PDF Summarizer")
 
     # title for the page
@@ -15,7 +20,16 @@ def main():
 
     submit = st.button("Generate Summary")
 
-    os.environ["OP"]
+    # Access the API key from environment variables
+    api_key = os.getenv("OPEN_API_KEY")
+    if api_key is None:
+        st.error("API Key is missing. Please check your .env file.")
+        return
+
+    if submit :
+        response = summarizer(pdf)
+        st.subheader('Summary of PDF File')
+        st.write(response)
 
 if __name__ == '__main__':
     main()
--- util.py
+++ util.py
@@ -2,11 +2,11 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain import FAISS
 from langchain.chains.question_answering import load_qa_chain
-from langchain.llms import openai
+from langchain.llms import OpenAI
 from langchain.chat_models import ChatOpenAI
 from langchain.callbacks import get_openai_callback
 
-from pypdf import PDFReader
+from pypdf import PdfReader
 
 def process_text(text):
     # process the given text splitting it into chunks and converting
@@ -14,7 +14,7 @@ def process_text(text):
     # Initialize a text splitter to divide the text into manageable chunks
     text_splitter = CharacterTextSplitter(
         separator = "\n",
-        chunk_sizee = 1000,
+        chunk_size = 1000,
         chunk_overlap = 200,
         length_function = len
     )
@@ -22,36 +22,37 @@ def process_text(text):
     chunks = text_splitter.split_text(text)
 
     #load a model for generating embeddings from Hugging Face
-    HuggingFaceEmbeddings = HuggingFaceEmbeddings(model_name = 'sentence-transformers/all=MiniLM-L6-v2')
+    embeddings_model  = HuggingFaceEmbeddings(model_name = 'sentence-transformers/all=MiniLM-L6-v2')
 
     # Create a FAISS indec from the text chunks from the text chunks using the embeddings
-    knowledgeBase = FAISS.from_texts(chunks, HuggingFaceEmbeddings)
+    knowledgeBase = FAISS.from_texts(chunks, embeddings_model )
 
     return knowledgeBase
 
 
 def summarizer(pdf):
+    # Function to summarize the content of a PDF File.
 
     if pdf is not None:
         # If a PDF File is provided
 
         # Read the PDF File
-        pdf_reader = pdf_reader(pdf)
+        reader = PdfReader(pdf)
         text = "" 
 
         # Extract text from each page of the PDF
-        for page in pdf_reader.pages:
+        for page in reader.pages:
             text += page.extract_text() or ""
 
         # Process the extracted text to create a knowledge base
         knowledgeBase = process_text(text)
 
         # Define the query fir summarization 
-        query = "Summarizee the content of the uploaded PDF file in appropriately 3-5 sentences."
+        query = "Summarize the content of the uploaded PDF file in appropriately 3-5 sentences."
 
         if query:
             # Perform a similarity search in the knowledge base using the query
-            docs = knowledgeBase.similarity_searcch(query)
+            docs = knowledgeBase.similarity_search(query)
 
             # Specify the model to use for generating the summary
             OpenAIModel = "gpt-3.5-turbo-16k"
@@ -60,10 +61,15 @@ def summarizer(pdf):
             # load a question-answering chain with the specifies model
             chain = load_qa_chain(llm, chain_type = 'stuff')
 
-            with get_openai_callback() as cost:
-                # Run the chain to get a response and track the code
-                response = chain.run(input_documents = docs, question = query)
-                # Print the cost of the operation
-                print(cost)
-                # Return the generated summary
-                return response
+            # with get_openai_callback() as cost:
+            #     # Run the chain to get a response and track the code
+            #     response = chain.run(input_documents = docs, question = query)
+            #     # Print the cost of the operation
+            #     print(cost)
+            #     # Return the generated summary
+            #     return response
+
+            # Run the chain to get a response and track the code
+            response = chain.run(input_documents = docs, question = query)
+            # Return the generated summary
+            return response 
